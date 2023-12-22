d53067fffdf013ee8211a4f85fa09047ae3036f4
K. Thea <dev.fwslw@simplelogin.fr>
2023-12-23 00:54:58 +0100

    Create basic app

--- noteman/noteman/settings.py
+++ noteman/noteman/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-=ye_aazt-^19swl81&2mf9m55!siw*lem)u$!1g#%49$cn@!x7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.19']
+ALLOWED_HOSTS = ['192.168.1.19', 'localhost']
 
 
 # Application definition
