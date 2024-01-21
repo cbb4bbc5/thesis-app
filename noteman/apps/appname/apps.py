bc2d9b957cef744643b0722234b04972a3bad4a3
K. Thea <dev.fwslw@simplelogin.fr>
2024-01-21 18:32:21 +0100

    Remove unnecessary files
    I deleted a Django app that I know I will not be using and with some
    files that I prepared for packaging because right now I am not
    interested in that as well.

--- noteman/apps/appname/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppnameConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.appname'
-    # I had to change it from appname to apps.appname!!!
