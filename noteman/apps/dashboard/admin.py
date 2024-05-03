2f3e493509370dd757bb235d8b8e0e7ef3ed9877
K. Thea <dev.fwslw@simplelogin.fr>
2024-05-03 17:24:52 +0200

    register models in admin.py

--- noteman/apps/dashboard/admin.py
+++ noteman/apps/dashboard/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Note, Connection, Tag, NoteTag
 
 # Register your models here.
+
+admin.site.register(Note)
+admin.site.register(Connection)
+admin.site.register(Tag)
+admin.site.register(NoteTag)
