a21231e6c907c63b73290b5b19b307999e17c01b
K. Thea <dev.fwslw@simplelogin.fr>
2024-05-03 17:44:09 +0200

    add constraints to models

--- noteman/apps/dashboard/models.py
+++ noteman/apps/dashboard/models.py
@@ -1,4 +1,4 @@
-from datetime import datetime as dt
+import django.utils
 
 from django.db import models
 from django.db.models.functions import Now
@@ -8,11 +8,11 @@ class Note(models.Model):
     name = models.CharField(max_length=200)
     # db_default or default
     # https://stackoverflow.com/questions/77665050/what-does-the-new-field-db-default-of-django-5-imply-should-we-use-it-by-def
-    cr_time = models.DateTimeField(default=Now)
+    cr_time = models.DateTimeField(default=django.utils.timezone.now)
     # TODO: use choices for type_id, like here
     # https://stackoverflow.com/questions/48040008/django-restrict-data-that-can-be-given-to-model-field
     type = models.IntegerField()
-    description = models.CharField(max_length=200)
+    description = models.CharField(max_length=200, blank=True)
 
 """Example code (copy for tests)
 import apps.dashboard.models
@@ -24,18 +24,46 @@ n3 = apps.dashboard.models.Note.objects.get_or_create(name='Third note', type=1,
 t1 = apps.dashboard.models.Tag.objects.get_or_create(name='red')[0]
 t2 = apps.dashboard.models.Tag.objects.get_or_create(name='blue')[0]
 t3 = apps.dashboard.models.Tag.objects.get_or_create(name='green')[0]
+but better to create it from the dashboard
 """
 
 
 class Connection(models.Model):
-     note = models.ForeignKey(Note, on_delete=models.CASCADE, related_name='note2note')
+     note = models.ForeignKey(Note, on_delete=models.CASCADE, related_name='note')
      source = models.ForeignKey(Note, on_delete=models.CASCADE, related_name='source')
-     comment = models.CharField(max_length=200)
+     comment = models.CharField(max_length=200, blank=True)
+
+     class Meta:
+         # https://stackoverflow.com/questions/63649333/how-to-add-constraint-for-2-fields-not-having-the-same-value
+         # TODO: also take into account avoiding cycles but probably not as a constraint? I need to think this through
+         constraints = [
+             models.CheckConstraint(
+                 check=~models.Q(note=models.F('source')),
+                 name='not_equal'
+             ),
+             models.UniqueConstraint(
+                 fields=['note', 'source'], name='unique_sources'
+             )
+         ]
 
 
 class Tag(models.Model):
     name = models.CharField(max_length=200, unique=True)
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['name'], name='unique_tag_name'
+            )
+        ]
 
 class NoteTag(models.Model):
     note = models.ForeignKey(Note, on_delete=models.CASCADE)
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    class Meta:
+        constraints = [
+            # https://code.djangoproject.com/wiki/MultipleColumnPrimaryKeys
+            # good replacement for composite primary key in django
+            models.UniqueConstraint(
+                fields=['note', 'tag'], name='unique_tags'
+            ),
+        ]
