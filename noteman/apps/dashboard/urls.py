581760e65fe486a211756567cd453f49b5abc1ca
K. Thea <dev.fwslw@simplelogin.fr>
2024-05-03 20:31:41 +0200

    Create template and view for individual notes
    I created simple template containting absolute minimum for displaying
    information about notes. It still lacks information about tags and
    sources. Also error messages are missing but I will add them as I flesh
    out the rest.

--- noteman/apps/dashboard/urls.py
+++ noteman/apps/dashboard/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    # better to add a trailing slash
+    path('notes/<int:note_id>/', views.note_detail, name='note_detail'),
 ]
--- noteman/apps/dashboard/views.py
+++ noteman/apps/dashboard/views.py
@@ -1,8 +1,21 @@
 from django.http import HttpResponse
 from django.template import loader
 
+from .models import Note
 
 def index(request):
     template = loader.get_template('common/index.html')
     context = {}
+    return HttpResponse(template.render(context, request))
+
+
+def note_detail(request, note_id):
+    template = loader.get_template('dashboard/note.html')
+    note = Note.objects.get(pk=note_id)
+    # TODO: take errors into account
+    context = {
+        'note' : note,
+    }
+    # TODO: add display of tags
+    # TODO: add display of sources
     return HttpResponse(template.render(context, request))
