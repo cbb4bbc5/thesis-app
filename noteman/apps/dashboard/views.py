84fd17f7d5ee03a18245bcdd1ab7c22dfd7d1159
K. Thea <dev.fwslw@simplelogin.fr>
2024-05-05 20:39:52 +0200

    Add listing all notes and tags for specific note
    index.html now has link to page listing all notes in a very basic style
    while I also added displaying tags (as well in a basic manner) to
    individual note's template.

--- noteman/apps/dashboard/views.py
+++ noteman/apps/dashboard/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.template import loader
 
-from .models import Note
+from .models import Note, NoteTag, Tag
 
 def index(request):
     template = loader.get_template('common/index.html')
@@ -9,13 +9,24 @@ def index(request):
     return HttpResponse(template.render(context, request))
 
 
+def all_notes(request):
+    template = loader.get_template('dashboard/all_notes.html')
+    notes = Note.objects.all()
+    context = {
+        'notes' : notes,
+    }
+    return HttpResponse(template.render(context, request))
+
 def note_detail(request, note_id):
     template = loader.get_template('dashboard/note.html')
     note = Note.objects.get(pk=note_id)
+    note_tags = NoteTag.objects.filter(note__id=note_id)
+    # TODO: maybe rewrite getting tags in some more elegant way? like using something similar to values
+    tags = [nt.tag for nt in note_tags]
     # TODO: take errors into account
     context = {
         'note' : note,
+        'tags' : tags,
     }
-    # TODO: add display of tags
     # TODO: add display of sources
     return HttpResponse(template.render(context, request))
