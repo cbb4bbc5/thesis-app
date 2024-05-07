c6c39534ee4193830ad5de525f8bed66a5974ccb
K. Thea <dev.fwslw@simplelogin.fr>
2024-05-07 20:47:00 +0200

    Add django rest framework to the project
    Add this as a project dependency however it is not configured beyond
    that. This commit is just the first step in creating the API.

--- noteman/noteman/urls.py
+++ noteman/noteman/urls.py
@@ -24,4 +24,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('apps.dashboard.urls')),
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
 ]
