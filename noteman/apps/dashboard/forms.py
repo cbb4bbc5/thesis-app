c5b8628a80f87f6ca9f13df74d23cbabc90fd983
K. Thea <dev.fwslw@simplelogin.fr>
2024-05-13 22:24:03 +0200

    Create basic form for notes with tags
    Adding tags now works, and multiple tags can be added at the time of
    creation however only existing tags, new cannot be created along with
    the note. Also adding connections is not possible but that is subject to
    change in the future commits.

--- noteman/apps/dashboard/forms.py
+++ noteman/apps/dashboard/forms.py
@@ -4,7 +4,8 @@ from .models import Note, Tag
 
 
 class NoteForm(forms.ModelForm):
-    tags = forms.ModelMultipleChoiceField(queryset=Tag.objects.all())
+    tags = forms.ModelMultipleChoiceField(queryset=Tag.objects.all(),
+                                          widget=forms.CheckboxSelectMultiple)
     class Meta:
         model = Note
         fields = [
@@ -12,5 +13,14 @@ class NoteForm(forms.ModelForm):
             'cr_time',
             'type',
             'description',
-            'tags',
         ]
+
+    def save(self):
+        # https://stackoverflow.com/questions/2216974/django-modelform-for-many-to-many-fields
+        # creating custom save function was enough to savee tag info however I do not
+        # fully understand how it works, as I think manytomany should have been saved
+        # without having to modify save function
+        instance = forms.ModelForm.save(self)
+        instance.tags.clear()
+        instance.tags.add(*self.cleaned_data['tags'])
+        return instance
