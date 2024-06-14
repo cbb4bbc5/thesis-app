c65c6c2ccf756aa826b08e1eb3c736b813b61ff3
K. Thea <dev.fwslw@simplelogin.fr>
2024-06-14 13:43:16 +0200

    add comment field to serializer

--- noteman/apps/api/serializers.py
+++ noteman/apps/api/serializers.py
@@ -54,6 +54,7 @@ class ConnectionSerializer(serializers.ModelSerializer):
             'id',
             'note',
             'source',
+            'comment',
         ]
         read_only_fields = [
             'id',
